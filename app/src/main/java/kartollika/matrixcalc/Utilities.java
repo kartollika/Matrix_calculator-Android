7cef8a91fe9aa0eb2c5f1058038c26b4fed14ed6
kartollika <maksimow.dmitrij@yandex.ru>
2018-05-28 12:00:54 +0300

    Extracted some utilities

---
+++ app/src/main/java/kartollika/matrixcalc/Utilities.java
@@ -0,0 +1,19 @@
+package kartollika.matrixcalc;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by kartollikaa on 28.05.2018.
+ */
+
+public final class Utilities {
+
+    static Toast createShortToast(Context context, String text) {
+        return Toast.makeText(context, text, Toast.LENGTH_SHORT);
+    }
+
+    static Toast createLongToast(Context context, String text) {
+        return Toast.makeText(context, text, Toast.LENGTH_LONG);
+    }
+}
