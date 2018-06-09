aa66816323e9069a0fa89606196f395c7a8e7e93
kartollika <maksimow.dmitrij@yandex.ru>
2018-06-09 17:21:12 +0300

    Version 1.5 Changed method for checking updates from application Improved performance x2

--- app/src/main/java/kartollika/matrixcalc/UpdateCheckerService.java
+++ app/src/main/java/kartollika/matrixcalc/UpdateCheckerService.java
@@ -7,7 +7,9 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
@@ -22,6 +24,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class UpdateCheckerService extends Service {
 
@@ -79,31 +82,57 @@ public class UpdateCheckerService extends Service {
     }
 
     private void checkForUpdates() {
-        int curVersionCode = BuildConfig.VERSION_CODE;
-        Pair<Integer, String> latestVersion = null;
+        if (hasConnection()) {
 
-        while (latestVersion == null) {
-            latestVersion = downloadText();
-        }
+            int curVersionCode = BuildConfig.VERSION_CODE;
+            Pair<Integer, String> latestVersion = downloadText();
 
-        if (curVersionCode < latestVersion.first) {
-            if (calledFromSettings) {
-                sendNotification(latestVersion.second);
-                showToast(getApplicationContext(), getResources().getString(R.string.update_available) +
-                        "\n" + getResources().getString(R.string.update_available_from_settings_part2));
-                stopSelf();
+            if (curVersionCode < latestVersion.first) {
+                if (calledFromSettings) {
+                    sendNotification(latestVersion.second);
+                    showToast(getApplicationContext(), getResources().getString(R.string.update_available) +
+                            "\n" + getResources().getString(R.string.update_available_from_settings_part2));
+                    stopSelf();
+                }
+                if (notifsAtStartAllowed) {
+                    sendNotification(latestVersion.second);
+                }
+
+            } else {
+                if (calledFromSettings) {
+                    showToast(getApplicationContext(), getResources().getString(R.string.noUpdateAvailable));
+                }
             }
-            if (notifsAtStartAllowed) {
-                sendNotification(latestVersion.second);
-            }
-
         } else {
-            if (calledFromSettings) {
-                showToast(getApplicationContext(), getResources().getString(R.string.noUpdateAvailable));
-            }
+            showToast(getApplicationContext(), getResources().getString(R.string.conn_error));
         }
-
         stopSelf();
+    }
+
+    private static class LatestVersionGetter extends AsyncTask<Void, String, Object[]> {
+
+        private int versionCode = -1;
+        private String versionName = "";
+
+        @Override
+        protected Object[] doInBackground(Void... voids) {
+            try {
+                URL url = new URL("https://rawgit.com/kartollika/Matrix_calculator-Android/master/app/latest_version_new.txt");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setConnectTimeout(30000);
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String str;
+                while ((str = in.readLine()) != null) {
+                    String[] parts = str.split(" ");
+                    versionCode = Integer.valueOf(parts[0]);
+                    versionName = parts[1];
+                }
+                in.close();
+            } catch (Exception e) {
+                return null;
+            }
+            return new Object[]{versionCode, versionName};
+        }
     }
 
     @Override
@@ -111,38 +140,23 @@ public class UpdateCheckerService extends Service {
         return null;
     }
 
-    private Pair<Integer, String> downloadText() {
-        final String[] versionName = new String[1];
-        final int[] versionCode = new int[1];
-        versionName[0] = "";
-
-        synchronized (this) {
-            while (versionName[0].equals("")) {
-                new Thread(new Runnable() {
-                    public void run() {
-                        try {
-                            URL url = new URL("https://rawgit.com/kartollika/Matrix_calculator-Android/master/app/latest_version_new.txt");
-                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                            conn.setConnectTimeout(10000);
-                            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                            String str;
-                            while ((str = in.readLine()) != null) {
-                                String[] parts = str.split(" ");
-                                versionCode[0] = Integer.valueOf(parts[0]);
-                                versionName[0] = parts[1];
-                            }
-                            in.close();
-                        } catch (Exception e) {
-                            if (calledFromSettings) {
-                                showToast(getApplicationContext(), "Connection error");
-                            }
-                        }
-                    }
-                }).start();
-            }
+    private Pair<Integer, String> downloadText() throws NullPointerException {
+        Object[] info = new Object[2];
+        LatestVersionGetter latestVersionGetter = new LatestVersionGetter();
+        try {
+            info = latestVersionGetter.execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
 
-        return new Pair<>(versionCode[0], versionName[0]);
+        return new Pair<>((int) info[0], (String) info[1]);
+    }
+
+    private boolean hasConnection() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnectedOrConnecting();
     }
 
     private void sendNotification(String latestVersionName) {
